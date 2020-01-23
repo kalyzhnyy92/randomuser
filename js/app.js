e76a4ea9a304fe4d1d68240de3abe5f34e248d9e
kalyzhnyy92 <49996705+kalyzhnyy92@users.noreply.github.com>
2020-01-23 17:08:41 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+'use strict';
+
 // Preloader page
 function loadData() {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, 4000);
+    setTimeout(resolve, 5000);
   })
 }
 
@@ -9,10 +11,10 @@ loadData()
 .then(() => {
 	let preloaderEl = document.getElementById('preloader');
     	preloaderEl.classList.add('hidden');
-    	preloaderEl.classList.remove('visible');
+    	preloaderEl.classList.add('visible');
 });
 
-//Random user genertor
+// Random user genertor
 function createNode(element) {
   	return document.createElement(element);
 }
@@ -21,45 +23,59 @@ function append(parent, el) {
 	return parent.appendChild(el);
 }
 
-let div = document.getElementById('authors');
-let number = Math.round(Math.random()*100);
-let url = 'https://randomuser.me/api/?results=';
-let randomUsers = `${url}${number}`;
+let button = document.querySelector('.js-button');
+let title = document.querySelector('.title');
 
-fetch(randomUsers)
-	.then((resp) => resp.json())
-	.then(function(data) {
-let authors = data.results;
-	return authors.map(function(author) {
-		let li = createNode('li'),
-      		img = createNode('img'),
-      		p = createNode('p'),
-      		span = createNode('span'),
-      		b = createNode('b'),
-      		i = createNode('i'),
-      		code = createNode('code'),
-      		kbd = createNode('kbd'),
-      		samp = createNode('samp');
+button.addEventListener('click', loadUsers);
 
-  		img.src = `${author.picture.large}`;
-  		p.innerHTML = `${author.name.title} ${author.name.first} ${author.name.last}`;
-  		span.innerHTML = `${author.gender}`;
-  		b.innerHTML = `${author.phone}`;
-  		i.innerHTML = `${author.email}`;
-  		code.innerHTML = `${author.location.state}, ${author.location.city}, ${author.location.street.name}, ${author.location.street.number}`;
-  		kbd.innerHTML = `<span>Date of birth:</span> ${new Date(author.dob.date).toLocaleDateString()}`;
-  		samp.innerHTML = `<span>Date of registration:</span> ${new Date(author.registered.date).toLocaleDateString()}`;
-  		append(div, li);
-  		append(li, img);
-  		append(li, p);
-  		append(li, span);
-  		append(li, b);
-  		append(li, i);
-  		append(li, code);
-  		append(li, kbd);
-  		append(li, samp);
-	})
-})
-.catch(function(error) {
-	console.log(error);
-});
+function loadUsers(){
+
+  button.classList.add('hidden');
+  title.classList.add('active');
+  preloader.classList.add('visible');
+
+  let div = document.getElementById('authors');
+  let number = Math.floor(Math.random() * Math.floor(100));
+  let url = 'https://randomuser.me/api/?results=';
+  let randomUsers = `${url}${number}`;
+
+  fetch(randomUsers)
+    .then((resp) => resp.json())
+    .then(function(data) {
+  let authors = data.results;
+    return authors.map(function(author) {
+      let li = createNode('li'),
+            img = createNode('img'),
+            p = createNode('p'),
+            span = createNode('span'),
+            b = createNode('b'),
+            i = createNode('i'),
+            code = createNode('code'),
+            kbd = createNode('kbd'),
+            samp = createNode('samp');
+
+        img.src = `${author.picture.large}`;
+        p.innerHTML = `${author.name.title} ${author.name.first} ${author.name.last}`;
+        span.innerHTML = `${author.gender}`;
+        b.innerHTML = `${author.phone}`;
+        i.innerHTML = `${author.email}`;
+        code.innerHTML = `${author.location.state}, ${author.location.city}, ${author.location.street.name}, ${author.location.street.number}`;
+        kbd.innerHTML = `<span>Date of birth:</span> ${new Date(author.dob.date).toLocaleDateString()}`;
+        samp.innerHTML = `<span>Date of registration:</span> ${new Date(author.registered.date).toLocaleDateString()}`;
+        append(div, li);
+        append(li, img);
+        append(li, p);
+        append(li, span);
+        append(li, b);
+        append(li, i);
+        append(li, code);
+        append(li, kbd);
+        append(li, samp);
+    })
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+
+}
